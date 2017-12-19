4f26ec27dfdecba97834f48b0fa4e15029db9fa6
xinyuehdzt <7607315613@qq.com>
2017-12-20 00:22:59 +0800

    test ball game

---
+++ Test Project/Assets/scripts/player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+    private Rigidbody rigidbody;
+
+    public int force = 5;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        rigidbody.AddForce(new Vector3(h, 0, v)* force);
+
+    }
+}
